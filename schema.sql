97658137a8c1670c8d6dfc6f853320389bd6fcb9
Ajita Srivastava <ajitas4000@gmail.com>
2018-09-05 20:06:23 -0700

    fixing typo

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE products(
     department_name VARCHAR(30) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     stock_quantity INTEGER NOT NULL,
-    product_sales DECIMAL(10,2) DEAFULT 0,
+    product_sales DECIMAL(10,2) DEFAULT 0,
     PRIMARY KEY(product_id)
 );
 
